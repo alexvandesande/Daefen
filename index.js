c57216a1b47197f443f9356afd7bb139569bbc9b
Alex Van de Sande <avsa@ethereum.org>
2018-09-27 14:59:03 -0300

    remove bignum

--- index.js
+++ index.js
@@ -1,29 +1,27 @@
 /*
 Daefen
 ------
-
 A library for encoding/decoding large numbers into/from a pronounceable high-density string constructed from a base of 3456 syllables.
-
 
 */
 
 // Creates the syllable array
 var syllables = [];
-var consonants = 'bcdfghjklmnprstvwz'; // consonants that are unambiguous and easy to pronounce
-var vowels = 'aeiouy'; // vowels
+var consonants = "bcdfghjklmnprstvwz"; // consonants that are unambiguous and easy to pronounce
+var vowels = "aeiouy"; // vowels
 
 // Vowel + Consonant
 for (a = 0; a < vowels.length; a++) {
   for (b = 0; b < consonants.length; b++) {
-    syllables.push(vowels[a] + consonants[b] );
-  } 
+    syllables.push(vowels[a] + consonants[b]);
+  }
 }
 
 // Consonant + Vowel
 for (b = 0; b < consonants.length; b++) {
   for (a = 0; a < vowels.length; a++) {
     syllables.push(consonants[b] + vowels[a]);
-  } 
+  }
 }
 
 // Consonant + Vowel + Vowel
@@ -31,8 +29,8 @@ for (b = 0; b < consonants.length; b++) {
   for (a = 0; a < vowels.length; a++) {
     for (e = 0; e < vowels.length; e++) {
       syllables.push(consonants[b] + vowels[a] + vowels[e]);
-    }  
-  } 
+    }
+  }
 }
 
 // Consonant + Vowel + Consonant
@@ -40,8 +38,8 @@ for (b = 0; b < consonants.length; b++) {
   for (a = 0; a < vowels.length; a++) {
     for (c = 0; c < consonants.length; c++) {
       syllables.push(consonants[b] + vowels[a] + consonants[c]);
-    }  
-  } 
+    }
+  }
 }
 
 // Vowel + Consonant + Vowel
@@ -49,17 +47,17 @@ for (a = 0; a < vowels.length; a++) {
   for (b = 0; b < consonants.length; b++) {
     for (e = 0; e < vowels.length; e++) {
       syllables.push(vowels[a] + consonants[b] + vowels[e]);
-    }  
-  } 
+    }
+  }
 }
 
 // Quick function that converts a big Number object into an array of numbers for any chosen base
 function fromBase10(bigNum, base) {
   var result = [];
   if (bigNum == 0) return [0];
-  for (var i=bigNum; i > 0; i = i.dividedBy(base).floor()){
-   result.unshift(i.modulo(base).toFixed());
-  };
+  for (var i = bigNum; i > 0; i = Math.floor(i / base)) {
+    result.unshift(i % base);
+  }
   return result;
 }
 
@@ -69,43 +67,60 @@ function isConsonant(letter) {
 }
 
 // Converts an integer (passed as a string to avoid scientific notation issues)
-function toWords(numberAsString) {
-  let bn = new BigNumber(numberAsString.toString());
-  let numberArray = fromBase10(bn, syllables.length);
-  let result = '';
-  let lastWord = '';
+function toWords(number) {
+  let numberArray = fromBase10(number, syllables.length);
+  let result = "";
+  let lastWord = "";
   let n = 0;
   for (i = 0; i < numberArray.length; i++) {
     n = numberArray[i] || 0;
 
     lastWord = result.split(" ").slice(-1)[0];
 
-    if (result == ''  || lastWord.length == syllables[n].length 
-      || (lastWord.length < 5 && isConsonant(lastWord.slice(-1)) && isConsonant(syllables[n].slice(0,1))) ) {
+    if (
+      result == "" ||
+      lastWord.length == syllables[n].length ||
+      (lastWord.length < 5 &&
+        isConsonant(lastWord.slice(-1)) &&
+        isConsonant(syllables[n].slice(0, 1)))
+    ) {
       result += syllables[n];
     } else {
       result += " " + syllables[n];
     }
   }
-  return result.replace(/\b[a-z]/g,function(f){return f.toUpperCase();});
+  return result.replace(/\b[a-z]/g, function(f) {
+    return f.toUpperCase();
+  });
 }
 
 // Converts a valid phrase back into a string
 function fromWords(words) {
-  let wordArray = words.toLowerCase()
-    .replace(/[bcdfghjklmnprstvwz][bcdfghjklmnprstvwz]/gi,function(r){ let n = Math.floor(r.length/2); return r.substr(0,n) + ' ' + r.substr(n,n)})
-    .replace(/[a-z]{6}|[a-z]{4}/gi,function(r){ let n = Math.floor(r.length/2); return r.substr(0,n) + ' ' + r.substr(n,n)})
+  let wordArray = words
+    .toLowerCase()
+    .replace(/[bcdfghjklmnprstvwz][bcdfghjklmnprstvwz]/gi, function(r) {
+      let n = Math.floor(r.length / 2);
+      return r.substr(0, n) + " " + r.substr(n, n);
+    })
+    .replace(/[a-z]{6}|[a-z]{4}/gi, function(r) {
+      let n = Math.floor(r.length / 2);
+      return r.substr(0, n) + " " + r.substr(n, n);
+    })
     .split(" ");
-  let result = new BigNumber(0);
+  let result = 0;
 
   for (i = 0; i < wordArray.length; i++) {
     if (syllables.indexOf(wordArray[i]) < 0) return;
-    result = result.plus(new BigNumber(syllables.indexOf(wordArray[i])).times(new BigNumber(syllables.length).toPower(wordArray.length - i - 1))) 
+
+    result =
+      result +
+      syllables.indexOf(wordArray[i]) *
+        Math.pow(syllables.length, wordArray.length - i - 1);
   }
 
-  return result.toString(10);
+  return result;
 }
 
-var random = Math.floor(Math.random()*Math.pow(3456,5)).toString()
-toWords(random)
+var random = Math.floor(Math.random() * Math.pow(2, 55));
 
+toWords(random);
